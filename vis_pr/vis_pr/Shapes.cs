00b74c441e25eb1e41c6d56e5ee17172af89f36d
Artem <a.isackov@gmail.com>
2013-01-16 00:10:00 +0700

    add_class_Shapes

---
+++ vis_pr/vis_pr/Shapes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Text;
+
+namespace vis_pr
+{
+    public abstract class Shapes
+    {
+        public abstract string DescriptionString { get; }
+        public abstract void DrawWith(Graphics g, Pen p);
+        public abstract void SaveTo(StreamWriter sw);
+        public abstract bool IsNearTo(Point C);
+
+        protected float getDistance(Point A, Point B)
+        {
+            return (float)Math.Sqrt(Math.Pow(A.X - B.X, 2) + Math.Pow(A.Y - B.Y, 2));
+        }
+    }
+}
